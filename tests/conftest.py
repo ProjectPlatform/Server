2d0c96251317f55d3fed860a9f09ca7759c5ce63
Anton Shusharin <antonshusharin01@gmail.com>
2020-10-26 00:17:09 +0300

    Tests: load schema into database before testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,27 @@
 import pytest
 import backend
 from settings import TestSettings
+import pathlib
 
 
 @pytest.fixture
 async def db():
     await backend.init(TestSettings())
+    await backend.config.db.execute(
+        """
+        CREATE SCHEMA IF NOT EXISTS tests;
+        SET search_path TO tests;"""
+    )
+    p = pathlib.Path.cwd()
+    while not (p / "db").exists():
+        p = p.parent
+    schema_script = (p / "db" / "schema.sql").read_text()
+    await backend.config.db.execute(schema_script)
     yield backend.config.db
+
+    await backend.config.db.execute(
+        """
+        SET search_path TO public;
+        DROP SCHEMA tests CASCADE;"""
+    )
     await backend.config.db.close()
