8f42bdfa617161be1220a974b9c7022ce39bffe7
Anton Shusharin <antonshusharin01@gmail.com>
2020-10-25 22:40:18 +0300

    Initial support for settings

---
+++ settings.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings
+
+
+class Settings:
+    db_host: str = "localhost"
+    db_port: int = 5432
+    db_name: str = "projectplatform"
+    db_user: str = "projectplatform"
+    db_pass: str = "projectplatform"
+
+    class config:
+        env_prefix = "pp_"
+
+
+class TestSettings(Settings):
+    class Config:
+        env_prefix = "pp_test_"
