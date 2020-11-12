f1728cdcff591bb4e740d3f8b534ece9d0de24d4
Anton Shusharin <antonshusharin01@gmail.com>
2020-11-12 22:35:20 +0300

    Db: add tables for chats

--- app/app/db/schema.sql
+++ app/app/db/schema.sql
@@ -19,6 +19,11 @@ CREATE TABLE images (
     image_path varchar(256),
     thumbnail_path varchar(256)
 );
+CREATE TABLE documents (
+    id varchar(12) PRIMARY KEY,
+    name varchar(256) NOT NULL;
+path varchar(256 NOT NULL),
+);
 CREATE TABLE users (
     id varchar(12) PRIMARY KEY,
     nick varchar(64) UNIQUE NOT NULL,
@@ -66,6 +71,38 @@ CREATE TABLE connections (
     from_user_id varchar(12) REFERENCES users ON DELETE CASCADE NOT NULL,
     to_user_id varchar(12) REFERENCES users ON DELETE CASCADE NOT NULL,
     groups integer
+);
+CREATE TABLE chats (
+    id varchar(12) PRIMARY KEY,
+    name varchar(64) NOT NULL,
+    avatar_id varchar(12) REFERENCES images,
+    colour integer,
+    is_encrypted boolean NOT NULL,
+    is_personal boolean NOT NULL,
+    is_user_expandable boolean NOT NULL,
+    is_non_admin boolean NOT NULL,
+    non_removable_messages boolean NOT NULL,
+    non_modifiable_messages boolean NOT NULL,
+    auto_remove_messages boolean NOT NULL,
+    digest_messages boolean NOT NULL
+);
+CREATE TABLE chat_memberships (
+    id varchar(12) PRIMARY KEY,
+    user_id varchar(12) REFERENCES users NOT NULL ON DELETE CASCADE,
+    chat_id varchar(12) REFERENCES chats NOT NULL ON DELETE CASCADE,
+    is_admin boolean NOT NULL
+);
+CREATE TABLE messages (
+    id varchar(12) PRIMARY KEY,
+    chat_id varchar(12) REFERENCES chats NOT NULL ON DELETE CASCADE,
+    time timestamp NOT NULL,
+    author_id varchar(12) REFERENCES users ON DELETE SET NULL,
+    body text NOT NULL
+);
+CREATE TABLE message_tags (
+    id varchar(12),
+    message_id varchar(12) REFERENCES messages NOT NULL ON DELETE CASCADE,
+    tag varchar(64) NOT NULL
 );
 COMMIT;
 
