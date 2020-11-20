c76c003ffb4b51117439f95e33bb1f216ff4bda5
Kondrahin <tony.kondrahin@yandex.ru>
2020-11-20 23:27:37 +0300

    Create working JWT token

--- app/app/src/security.py
+++ app/app/src/security.py
@@ -1,15 +1,53 @@
+import os
 from datetime import datetime, timedelta
 from typing import Optional
 
+from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 
+from dotenv import load_dotenv
+from starlette import status
+
+from app.app.backend import get_user_info
+
+load_dotenv()
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-ALGORITHM = "HS256"
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-SECRET_KEY = "917eb105d213c6b8ae081adea18a508c72906ab98aac14684eb2cdd35806952c"
+ALGORITHM = os.environ.get("ALGORITHM")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+# def get_password_hash(password):
+#     return pwd_context.hash(password)
+
+
+async def decode_token(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    credentials_exception1 = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials123",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = int(payload.get("sub"))
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception1
+    user = await get_user_info(current_user=user_id, user_id=user_id)
+    if user is None:
+        raise credentials_exception1
+    return user
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
