4bd2e998eeb98ff6dffcf58c9d723e2b65fb1a2d
Kondrahin <tony.kondrahin@yandex.ru>
2020-11-20 23:25:54 +0300

    Minor refactoring

--- app/app/inital_data.py
+++ app/app/inital_data.py
@@ -3,24 +3,23 @@ import asyncio
 from app.app.settings import Settings
 import os
 from app.app.backend import init as init_db
-
+from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 async def init() -> None:
-    settings = Settings(db_host=os.environ["PP_DB_HOST"],
+    settings = Settings(db_host=os.environ.get("PP_DB_HOST"),
                         db_port=5432,
-                        db_name=os.environ["PP_DB_NAME"],
-                        db_user=os.environ["PP_DB_USER"],
-                        db_pass=os.environ["PP_DB_PASS"])
+                        db_name=os.environ.get("PP_DB_NAME"),
+                        db_user=os.environ.get("PP_DB_USER"),
+                        db_pass=os.environ.get("PP_DB_PASS"))
     logger.info("init db")
     await init_db(settings=settings)
 
 
 async def main() -> None:
-    from dotenv import load_dotenv
     dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     if os.path.exists(dotenv_path):
         load_dotenv(dotenv_path)
