ffb3ba5dd43339cca4ffc6a06e9c61868b6b03a7
Kondrahin <tony.kondrahin@yandex.ru>
2020-11-24 22:28:05 +0300

    add working token

--- app/app/api/users.py
+++ app/app/api/users.py
@@ -2,8 +2,9 @@ from typing import Optional
 
 from app.app.backend import ObjectNotFound
 from app.app.backend.user import get_user_info as get_info
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 from app.app.src.schemas import user
+from app.app.src.security import decode_token
 
 router = APIRouter()
 
@@ -12,9 +13,10 @@ router = APIRouter()
 
 
 @router.get("/get_user_info", response_model=user.UserOut)
-async def get_user_info(current_user: Optional[str], user_id: str):
+async def get_user_info(user_id: int, token: str = Depends(decode_token)):
     try:
-        user_info = await get_info(current_user=current_user, user_id=user_id)
+        current_user_id = token["id"]
+        user_info = await get_info(current_user=current_user_id, user_id=user_id)
         return user_info
     except ObjectNotFound:
         raise HTTPException(status_code=404, detail="Sorry, page not found")
