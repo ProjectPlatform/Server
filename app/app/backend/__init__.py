cbcb18a88e178acc665b230ebebfa3b0c0f14af9
Kondrahin <tony.kondrahin@yandex.ru>
2020-12-03 22:41:27 +0300

    add logging

--- app/app/backend/__init__.py
+++ app/app/backend/__init__.py
@@ -2,10 +2,6 @@ import asyncpg
 from app.app.settings import Settings
 from app.app.backend import config
 from app.app.backend.user import *
-import logging
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 
 async def init(settings: Settings):
