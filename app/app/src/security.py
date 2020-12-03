fa65c0a13b347ad3763d39a9c158be738b8b56ba
Kondrahin <tony.kondrahin@yandex.ru>
2020-12-03 22:51:40 +0300

    beginning of scopes and minor changes

--- app/app/src/security.py
+++ app/app/src/security.py
@@ -3,38 +3,35 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 from fastapi import HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
 from jose import jwt, JWTError
 from passlib.context import CryptContext
 
 from dotenv import load_dotenv
 from starlette import status
 
-from app.app.backend import get_user_info
+from app.app.backend import get_user_info, ObjectNotFound
 
 load_dotenv()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ALGORITHM = os.environ.get("ALGORITHM")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login",
+                                     scopes={"user": "Ordinary user", "developer": "application developer"})
 SECRET_KEY = os.environ.get("SECRET_KEY")
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 3000
 
 
 # def get_password_hash(password):
 #     return pwd_context.hash(password)
 
 
-async def decode_token(token: str = Depends(oauth2_scheme)):
+async def decode_token(security_scopes: SecurityScopes, token: str = Depends(oauth2_scheme)):
+    # if security_scopes.scopes
+
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    credentials_exception1 = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials123",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
@@ -43,10 +40,13 @@ async def decode_token(token: str = Depends(oauth2_scheme)):
         if user_id is None:
             raise credentials_exception
     except JWTError:
-        raise credentials_exception1
-    user = await get_user_info(current_user=user_id, user_id=user_id)
+        raise credentials_exception
+    try:
+        user = await get_user_info(current_user=user_id, user_id=user_id)
+    except ObjectNotFound:
+        raise credentials_exception
     if user is None:
-        raise credentials_exception1
+        raise credentials_exception
     return user
 
 
@@ -55,7 +55,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=3000)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
