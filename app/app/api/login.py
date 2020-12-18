c0112d561fd309ce9885cb6696bee9a59c4af36d
Kondrahin <tony.kondrahin@yandex.ru>
2020-12-18 23:34:58 +0300

    add verification code on email

--- app/app/api/login.py
+++ app/app/api/login.py
@@ -1,36 +1,37 @@
 import logging
+import random
 from datetime import timedelta
 from http.client import HTTPException
-from typing import Any
+from typing import Any, Optional
 
 from fastapi import Depends, HTTPException, status, Body
 from fastapi import APIRouter
 from fastapi.security import OAuth2PasswordRequestForm
 from firebase_admin import auth, messaging
+from starlette.responses import JSONResponse
 
 from app.app.backend.exceptions import NotInitialised
 from app.app.src import schemas
-from app.app.backend import user, NickTaken, EmailTaken, AuthenticationError, ObjectNotFound
-from app.app.backend.user import insert_fcm_token
-from app.app.src.schemas.user import UserAuth
+from app.app.backend import user, NickTaken, EmailTaken, AuthenticationError, ObjectNotFound, get_user_info
+from app.app.backend.user import insert_fcm_token, delete_fcm_token, check_duplicate_fcm_token, temporary_registration,\
+    verification_attempt
 from app.app.src.security import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES, decode_token
-from app.app import inital_data
+from app.app.utils import send_verification_email
 
 router = APIRouter()
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
 
 @router.post("/login", response_model=schemas.TokenOut)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), ):
+async def login_for_access_token(fcm_token: Optional[str] = None, form_data: OAuth2PasswordRequestForm = Depends(), ):
     """
     **Authenticate a user**
 
     Return the **id** of the authenticated account
 
     **Exceptions**
-    * Status code **400**
     * Status code **401**
     * Status code **501**
     """
@@ -47,11 +48,14 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
             data={"sub": str(user_id), "scopes": form_data.scopes},
             expires_delta=access_token_expires
         )
-        # custom_token = auth.create_custom_token(str(user_id))
-        # logging.info(custom_token)
+
+        if fcm_token is not None:
+            await check_duplicate_fcm_token(fcm_token=fcm_token)
+            await insert_fcm_token(current_user=user_id, fcm_token=fcm_token)
+
         return {"access_token": access_token, "token_type": "bearer", "id": user_id}
     except NotInitialised:
-        HTTPException(status_code=501, detail="Sorry, database was not initialized")
+        HTTPException(status_code=501, detail="Sorry, we have some problems on server")
     except AuthenticationError:
         raise HTTPException(status_code=401, detail="Invalid username or password")
 
@@ -71,25 +75,64 @@ async def create_user(
     """
     try:
         await user.register(nick=user_in.nick, password=user_in.password, email=user_in.email, name=user_in.name)
-        # Add email send
-        return {"status": "ok"}
+        user_info = await get_user_info(user_nick=user_in.nick)
+
+        verification_code = random.randint(100000, 999999)
+        await send_verification_email(email_to=user_in.email, subject_template="Please verify your email",
+                                      html_template="""
+                                                <mjml>
+                                          <mj-body background-color="#fff">
+                                            <mj-section>
+                                              <mj-column>
+                                                <mj-divider border-color="#555"></mj-divider>
+                                                <mj-text font-size="20px" color="#555" font-family="helvetica">Your verification code: {{ code }}</mj-text>
+                                        
+                                                <mj-divider border-color="#555" border-width="2px" />
+                                              </mj-column>
+                                            </mj-section>
+                                          </mj-body>
+                                        </mjml>""",
+                                      environment={"code": verification_code})
+
+        await temporary_registration(user_id=user_info["id"], verification_code=verification_code)
+        return {"id": user_info["id"]}
     except NotInitialised:
-        HTTPException(status_code=501, detail="Sorry, database was not initialized")
+        HTTPException(status_code=501, detail="Sorry, we have some problems on server")
     except NickTaken:
         raise HTTPException(status_code=422, detail="Nick is taken")
     except EmailTaken:
         raise HTTPException(status_code=422, detail="Email is taken")
 
 
-# @router.post("/get_fcm_token")
-# async def get_fcm_token(token: str= Body()):
-#     if
+@router.post("/verification")
+async def verification(user_id: int, verification_code: int):
+    """
+    **Verify a user account.**
 
-@router.post("/send_fcm_token")
-async def send_fcm_token(fcm_token: str, token: str = Depends(decode_token), ):
+    Returns JSON string with the field ```message```
+    In case of successful verification status_code = 200 else 422
+
+    """
+    result = await verification_attempt(user_id=user_id, verification_code=verification_code)
+    if result:
+        return JSONResponse(content={"message": "You have successfully verified your account"})
+    else:
+        return JSONResponse(status_code=422, content={"message": "Wrong verification code. Try again"})
+
+
+@router.post("/logout")
+async def logout(fcm_token: str, token: str = Depends(decode_token)):
+    """
+    **Revokes the user's fcm token.**
+
+    Returns JSON string ```{'result': true}```
+
+    **Exceptions:**
+    * Status code **404**
+    """
     try:
         user_id = token["id"]
-        await insert_fcm_token(current_user=user_id, fcm_token=fcm_token)
+        await delete_fcm_token(current_user=user_id, fcm_token=fcm_token)
         return {"result": True}
     except ObjectNotFound:
         raise HTTPException(status_code=404, detail="Page not found")
@@ -97,7 +140,7 @@ async def send_fcm_token(fcm_token: str, token: str = Depends(decode_token), ):
 
 @router.get("/echo")
 async def read_root(echo: str):
-    logger.info(echo)
+    log.info(echo)
     message = messaging.Message(
         notification=messaging.Notification(
             title=echo,
--- app/app/backend/user.py
+++ app/app/backend/user.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Optional, Dict, Any
 
 from asyncpg.exceptions import UniqueViolationError
@@ -8,11 +9,13 @@ from app.app.backend import config
 from app.app.backend.exceptions import NickTaken, EmailTaken, AuthenticationError, ObjectNotFound
 from app.app.backend.utils import db_required, insert_with_unique_id
 
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
+
 
 @db_required
 async def register(nick: str, password: str, email: EmailStr, name: str):
     password_hash = pbkdf2_sha256.hash(password)
-    print(password_hash)
     try:
         return await insert_with_unique_id(
             "users_authentication",
@@ -30,7 +33,6 @@ async def register(nick: str, password: str, email: EmailStr, name: str):
 async def authenticate(nick: str, password: str) -> str:
     if user := await config.db.fetchrow(
             "SELECT id, passwd_hash FROM users_authentication WHERE nick = $1;", nick
-            # "SELECT id, password FROM users WHERE nick = $1", nick
     ):
         if pbkdf2_sha256.verify(password, user["passwd_hash"]):
             return user["id"]
@@ -38,11 +40,12 @@ async def authenticate(nick: str, password: str) -> str:
 
 
 @db_required
-async def get_user_info(current_user: Optional[int] = None, user_id: Optional[int] = None, user_nick: Optional[int] = None,
+async def get_user_info(current_user: Optional[int] = None, user_id: Optional[int] = None,
+                        user_nick: Optional[str] = None,
                         user_email: Optional[EmailStr] = None) -> Dict[str, Any]:
     if user_id is not None:
         if u := await config.db.fetchrow("SELECT * FROM users_authentication WHERE id=$1", user_id):
-            return {"id": u["id"], "nick": u["nick"], "name": u["name"]}
+            return {"id": u["id"], "nick": u["nick"], "name": u["name"], "email": u["email"]}
             # TODO Ilya will add new table user info which will be contain column avatar_id
             # return {"nick": u["nick"], "name": u["name"], "avatar_id": u["avatar_id"]}
         else:
@@ -57,6 +60,31 @@ async def get_user_info(current_user: Optional[int] = None, user_id: Optional[in
             return {"id": u["id"], "nick": u["nick"], "name": u["name"]}
         else:
             raise ObjectNotFound()
+
+
+@db_required
+async def temporary_registration(user_id: int, verification_code: int):
+    await config.db.execute(f'insert into registration_verification_codes(id, mailCode) values ($1, $2);',
+                            user_id,
+                            verification_code)
+
+
+@db_required
+async def verification_attempt(user_id: int, verification_code: int):
+    result = await config.db.fetchrow(
+        f'select attemptsMade, (mailCode = $2)::bool from registration_verification_codes '
+        f'where id = $1 and validTime > current_timestamp limit 1;',
+        user_id,
+        verification_code)
+    if result:
+        await config.db.execute(f'delete from registration_verification_codes where id = $1;',
+                                user_id)
+        await config.db.execute(f'update users_authentication set was_confirmed = true where id = $1;',
+                                user_id)
+    else:
+        await config.db.execute(f'update registration_verification_codes set attemptsMade = attemptsMade + 1 where id = $1;',
+                                user_id)
+    return result
 
 
 @db_required
@@ -91,13 +119,29 @@ async def change_email(current_user: int, email: EmailStr):
 
 @db_required
 async def insert_fcm_token(current_user: int, fcm_token: str):
-    # try:
     await config.db.execute("UPDATE users_authentication SET devices_token_list ="
                             " array_append(users_authentication.devices_token_list, $2::varchar) "
                             "WHERE ID = $1 AND NOT ($2= ANY(users_authentication.devices_token_list));",
                             current_user,
                             fcm_token,
                             )
-    return {'result': True}
-# except Exception as e:
-#     raise ObjectNotFound()
+
+
+@db_required
+async def delete_fcm_token(current_user: int, fcm_token: str):
+    try:
+        await config.db.execute(
+            "update users_authentication set devices_token_list = array_remove(devices_token_list, $2::varchar) where id = $1 and $2 = any(devices_token_list);",
+            current_user,
+            fcm_token,
+        )
+    except Exception as e:
+        raise ObjectNotFound()
+
+
+@db_required
+async def check_duplicate_fcm_token(fcm_token: str):
+    await config.db.execute(
+        "update users_authentication set devices_token_list = array_remove(devices_token_list, $1::varchar);",
+        fcm_token,
+    )
