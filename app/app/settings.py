3849be966d2a5450bbba69b027ba6e3ee5535309
Kondrahin <tony.kondrahin@yandex.ru>
2020-12-30 23:10:55 +0300

    move to .ini

--- app/app/settings.py
+++ app/app/settings.py
@@ -1,14 +1,20 @@
+import os
+from configparser import ConfigParser
+
 from pydantic import BaseSettings
+
+config = ConfigParser()
+config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 
 
 class Settings(BaseSettings):
-    db_host: str = "localhost"
+    db_host: str = config['db']['PP_DB_HOST']
     db_port: int = 5432
-    db_name: str = "projectplatform"
-    db_user: str = "projectplatform"
-    db_pass: str = "projectplatform"
+    db_name: str = config['db']['PP_DB_NAME']
+    db_user: str = config['db']['PP_DB_USER']
+    db_pass: str = config['db']['PP_DB_PASS']
 
-    class config:
+    class сonfig:
         env_prefix = "pp_"
 
 
